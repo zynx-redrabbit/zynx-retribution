40cdab023fb6898940ac758b183ba58f55f7862b
zynx <zynx_games@outlook.com>
2025-05-23 17:42:12 -0700

    animation interaction wip

--- scripts/zynx.gd
+++ scripts/zynx.gd
@@ -1,1 +1,12 @@
 extends CanvasGroup
+
+enum Parts {BODY, HEAD}
+enum ShellType {DEFAULT, WING, FIRE, BAT, ROCKET}
+
+@export var character : Array[AnimatedSprite2D]
+@export var colliders : Array[Area2D]
+@export var shell_type: ShellType
+
+func _on_col_body_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		print("!")
